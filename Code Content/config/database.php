b5e91387e7034eb78844da49ccdc7571dcadde95
Siamak Samie <s_samie@bidden.encs.concordia.ca>
2017-02-10 12:20:57 -0500

    Minimizing excess code, implemented angularJS side nav

--- Code Content/config/database.php
+++ Code Content/config/database.php
@@ -41,10 +41,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ssamie-soen341-4343809'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'test'),
-            'username' => env('DB_USERNAME', 'root'),
+            'username' => env('DB_USERNAME', 'ssamie'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
