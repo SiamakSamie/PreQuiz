392f30632d01b946d33c32a627db8262746aa6bb
Roy Khoury <roy_khoury12@hotmail.com>
2017-03-02 14:54:05 -0500

    Functional about us page, with nice layout

--- Code Content/resources/views/aboutus.blade.php
+++ Code Content/resources/views/aboutus.blade.php
@@ -5,18 +5,53 @@
 @endsection
 
 @section('content')
+<div class="container">
   <div class= "row">
-      <h1>
+      <h1 class = "page-header">
         About us
         <small class="text-muted">Hey! We are happy to meet you !</small>
       </h1>
   </div>
   <div class="row">
       <p class="lead">
-        We are a team of * engineering student who are creating a user friendly webiste that will allow a student to re-familiarize themselves with necessary knowledge they may have forgotten that would be pertinent to the course.
+        We are a team of 8 engineering students who are creating a user friendly webiste that will allow a student to re-familiarize themselves with necessary knowledge they may have forgotten that would be pertinent to the course.
       </p>
-      <h1>
-        Our Team
-      </h1>
+      </div>
+      <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Our Team</h1>
+            </div>
+        <div class="col-xs-4">
+            <img class="img-responsive img-circle img-center" src="https://www.engr.wisc.edu/app/uploads/2015/12/Twitter-Logo-Circle-Small-300x300.png" />
+            <h4> Name of the person <small> Job function</small></h4>
+              <p>gsjsfjghjsdhgksjd;khd;kg;kdhfgkds;kgksd;gksjhfgh</p>
+        </div>
+        <div class="col-xs-4">
+            <img class="img-responsive img-circle img-center" src="https://www.engr.wisc.edu/app/uploads/2015/12/Twitter-Logo-Circle-Small-300x300.png" />
+            <h4> Name of the person <small> Job function</small></h4>
+              <p>gsjsfjghjsdhgksjd;khd;kg;kdhfgkds;kgksd;gksjhfgh</p>
+        </div>
+        <div class="col-xs-4">
+            <img class="img-responsive img-circle img-center" src="https://www.engr.wisc.edu/app/uploads/2015/12/Twitter-Logo-Circle-Small-300x300.png" />
+            <h4> Name of the person <small> Job function</small></h4>
+              <p>gsjsfjghjsdhgksjd;khd;kg;kdhfgkds;kgksd;gksjhfgh</p>
+        </div>
+        <div class="col-xs-4">
+            <img class="img-responsive img-circle img-center" src="https://www.engr.wisc.edu/app/uploads/2015/12/Twitter-Logo-Circle-Small-300x300.png" />
+            <h4> Name of the person <small> Job function</small></h4>
+              <p>gsjsfjghjsdhgksjd;khd;kg;kdhfgkds;kgksd;gksjhfgh</p>
+        </div>
+        <div class="col-xs-4 ">
+            <img class="img-responsive img-circle img-center" src="https://www.engr.wisc.edu/app/uploads/2015/12/Twitter-Logo-Circle-Small-300x300.png" />
+            <h4> Name of the person <small> Job function</small></h4>
+              <p>gsjsfjghjsdhgksjd;khd;kg;kdhfgkds;kgksd;gksjhfgh</p>
+        </div>
+        <div class="col-xs-4 text-center">
+            <img class="img-responsive img-circle img-center" src="https://www.engr.wisc.edu/app/uploads/2015/12/Twitter-Logo-Circle-Small-300x300.png" />
+            <h4> Name of the person <small> Job function</small></h4>
+              <p>gsjsfjghjsdhgksjd;khd;kg;kdhfgkds;kgksd;gksjhfgh</p>
+        </div>
+        
   </div>
-@endsection	
+</div>
+@endsection		
