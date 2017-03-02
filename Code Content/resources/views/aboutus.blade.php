ce0673765d7c33440524b3a9f954ed6c489d0505
adamrais <adamrais14@gmail.com>
2017-03-02 13:45:50 -0500

    Create aboutus.blade.php
    About us page, temporary template for travis test

---
+++ Code Content/resources/views/aboutus.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title')
+    About us
+@endsection
+
+@section('content')
+  <div class= "row">
+      <h1>
+        About us
+        <small class="text-muted">Hey! We are happy to meet you !</small>
+      </h1>
+  </div>
+  <div class="row">
+      <p class="lead">
+        We are a team of * engineering student who are creating a user friendly webiste that will allow a student to re-familiarize themselves with necessary knowledge they may have forgotten that would be pertinent to the course.
+      </p>
+      <h1>
+        Our Team
+      </h1>
+  </div>
+@endsection	
