efc2f605a5f09a6ece9468f3de0a4de0e6070eca
Josephine Ng <josephineng@Josephines-MacBook-Pro.local>
2017-03-09 23:25:43 -0500

    trying to fix travix try no2

--- Code Content/storage/framework/views/6d68b8d1b9a5a1ba76e2f20ad9b1fdb6ea96f3eb.php
+++ Code Content/storage/framework/views/6d68b8d1b9a5a1ba76e2f20ad9b1fdb6ea96f3eb.php
@@ -4,59 +4,57 @@
  <?php $__env->stopSection(); ?>
  
  <?php $__env->startSection('extra_links'); ?>
+ 
  <?php $__env->stopSection(); ?>
  
  <?php $__env->startSection('content'); ?>
-   <div class="container" style = "padding-top:50px">
-    <h1>Profile information</h1>
-  	<hr>
-	<div class="row">
-      <!-- left column -->
-      <div class="col-md-3">
-        <div class="text-center">
-          <img src="/img/profile_pic.jpg" class="avatar img-circle" alt="avatar">
-          <h6>Default profile picture</h6>
-          
-        </div>
+     
+     <div class = "page-header">
+         
+         <h1 class="text-center">
+             Profile page information
+         </h1>
+         
+     </div>
+     <?php $__currentLoopData = $user_info; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $info): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+     <div class="container flex-center">
+          <table class="table table-bordered ">
+              <thead>
+                  <tr>
+                      <th>
+                         University
+                      </th>
+                      <th>
+                          Name
+                      </th>
+                      <th>
+                          Email
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+              <tr>
+                  <td>  <?php echo e($info->university); ?> </td>
+                  <td>  <?php echo e($info->name); ?> </td>
+                  <td>  <?php echo e($info->email); ?> </td>
+                  
+              </tr>
+              <tr>
+                  <td>  <?php echo e($info->university); ?> </td>
+                  <td>  <?php echo e($info->name); ?> </td>
+                  <td>  <?php echo e($info->email); ?> </td>
+              </tr>
+              <tr>
+                  <td> <?php echo e($info->email); ?> </td>
+                  <td> <?php echo e($info->university); ?> </td>
+                  <td> <?php echo e($info->name); ?> </td>
+              </tr>
+              </tbody>
+          </table>
       </div>
-      
-      <!-- profile information -->
-      <div class="col-md-9 personal-info">
-       
-        <h3><b>Personal info</b></h3>
-        
-         <?php $__currentLoopData = $user_info; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $info): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <form class="form-horizontal" role="form" action = "/EditProfile" method ="POST">
-          <?php echo e(csrf_field()); ?>
-
-          
-          <div class="form-group">
-            <label class="col-lg-3 control-label">Name:</label>
-            <p> <?php echo e($info->name); ?></p>
-          </div>
-          <div class="form-group">
-            <label class="col-lg-3 control-label">University:</label>
-            <p>Some university</p>
-          </div>
-          <div class="form-group">
-            <label class="col-lg-3 control-label">Email:</label>
-            <p><?php echo e($info->email); ?></p>
-          </div>
-          <div class="form-group">
-            <label class="col-md-3 control-label">Password:</label>
-            <p>************</p>
-          </div>
-          <div class="form-group">
-            <label class="col-md-3 control-label"></label>
-            <div class="col-md-8">
-              <input type="submit" class="btn btn-primary"  value="Edit Profile">
-            </div>
-          </div>
-        </form>
-          <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-      </div>
-  </div>
-</div>
-<hr>
+      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+     <button type="button" class="btn btn-primary btn-lg btn-block"> Edit Information
+     </button>
+           
  <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
