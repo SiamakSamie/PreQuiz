c4716674483e6c696c9940f01917f08c3a591aab
Ryan Nichols <ryan3nichols@gmail.com>
2017-03-17 12:01:03 -0400

    the contact us email is now sending the correct subject line and content of email.-fixes issue #92

--- Code Content/routes/web.php
+++ Code Content/routes/web.php
@@ -15,7 +15,10 @@ Route::post('/getAllUnis', 'SearchController@getAllUnis');
 
 Route::post('/getAllCourses', 'SearchController@getAllCourses');
 
+Route::post('/getAllUserNames', 'SearchController@getAllUsernames');
+
 Route::get('/', function () {
+    
     return view('home');
 });
 
@@ -25,26 +28,42 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-Route::get('/profile', 'ProfileController@displayAll');
 
-Route::resource('create_quiz','QuizController');
-Route::resource('questions','QuestionsController');
+Route::get('/profile/{id}', 'ProfileController@displayAll');
+
+
+Route::group( ['middleware' => 'auth' ], function()
+{
+    Route::resource('create_quiz','QuizController');
+    Route::resource('questions','QuestionsController');
+    Route::post('/EditProfile', 'EditProfileController@update');
+    Route::post('/addComment', 'CommentController@addComment');
+    Route::get('/profile', 'ProfileController@displayAll');
+});
+
+Route::get('/take_quiz', 'TakeQuizController@take_quiz');
 
 Route::get('/contactus', 'ContactUsController@contactus');
 
-Route::get('/aboutus', function(){
-    return view('aboutus');
-});
+Route::get('/aboutus', function(){return view('aboutus');});
+
+Route::post('/EditProfile', 'EditProfileController@display');
+
+Route::post('/updateProfileInfo', "EditProfileController@update");
+
+Route::post('/addComment', 'CommentController@addComment');
 
 Route::post('/sendContactUsMail', function() {
    $data = request("message");
    $name = request("name");
    $email = request("email");
    
-   \Mail::send('email_message', array('name'=>$name), function($message) {
-       $message->to("ryan3nichols@gmail.com", "contact us")->subject("subject");
- 
+   Mail::send('email_message', array('name'=>$name, 'data'=>$data, 'email'=>$email), function($message) {        
+     $message->to("ryan3nichols@gmail.com", "contact us")->subject("Contact Us email from PreQuiz"); 
+     
    });
 
    return [$data,$name,$email];
-});
+
+ });  
+
