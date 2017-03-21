0d47992e8b2797b3f3098b6110c97911d134a542
SunXP <SunXP@users.noreply.github.com>
2017-03-21 17:07:07 -0400

    validation for taking a quiz
    Added checking right or wrong answers with predefined set of questions and correct answers.

--- Code Content/resources/views/take_quiz.blade.php
+++ Code Content/resources/views/take_quiz.blade.php
@@ -17,67 +17,23 @@
     
       <md-content>
         <md-tabs md-dynamic-height="" md-border-bottom="">
-          <md-tab label="Question 1">
-            <md-content class="md-padding">
-              <h1 class="md-display-1">How much wood could a wood chuck chuck if a wood could chuck wood?</h1>
-              <md-radio-group flex>
-                  <md-radio-button value='1'>about all of it</md-radio-button>
-                  <md-radio-button value='2'>What's a woodchuck? A beaver? </md-radio-button>
-                  <md-radio-button value='3'>depends on the time of the year</md-radio-button>
-                  <md-radio-button value='4'>14.5</md-radio-button>
-              </md-radio-group>
-              
-              <md-button class="md-raised md-primary btn-block">Check Answer</md-button>
-            </md-content>
-          </md-tab>
-          <md-tab label="Question 2">
-            <md-content class="md-padding">
-              <h1 class="md-display-1">How much wood could a wood chuck chuck if a wood could chuck wood?</h1>
-              <md-radio-group flex>
-                  <md-radio-button value='1' class="bg-danger" disabled>about all of it</md-radio-button>
-                  <md-radio-button value='2'disabled>What's a woodchuck? A beaver? </md-radio-button>
-                  <md-radio-button value='3'disabled>depends on the time of the year</md-radio-button>
-                  <md-radio-button value='4' class="bg-success"disabled>14.5</md-radio-button>
-              </md-radio-group>
-              <md-button class="md-raised md-primary btn-block">Check Answer</md-button>
-            </md-content>
-          </md-tab>
-          <md-tab label="Question 3">
-            <md-content class="md-padding">
-              <h1 class="md-display-1">How much wood could a wood chuck chuck if a wood could chuck wood?</h1>
-              <md-radio-group flex>
-                  <md-radio-button value='1'>about all of it</md-radio-button>
-                  <md-radio-button value='2'>What's a woodchuck? A beaver? </md-radio-button>
-                  <md-radio-button value='3'>depends on the time of the year</md-radio-button>
-                  <md-radio-button value='4'>14.5</md-radio-button>
-              </md-radio-group>
-              <md-button class="md-raised md-primary btn-block">Check Answer</md-button>
-            </md-content>
-          </md-tab>
-          <md-tab label="Question 4">
-            <md-content class="md-padding">
-              <h1 class="md-display-1">How much wood could a wood chuck chuck if a wood could chuck wood?</h1>
-              <md-radio-group flex>
-                  <md-radio-button value='1'>about all of it</md-radio-button>
-                  <md-radio-button value='2'>What's a woodchuck? A beaver? </md-radio-button>
-                  <md-radio-button value='3'>depends on the time of the year</md-radio-button>
-                  <md-radio-button value='4'>14.5</md-radio-button>
-              </md-radio-group>
-              <md-button class="md-raised md-primary btn-block">Check Answer</md-button>
-            </md-content>
-          </md-tab>              
-          <md-tab label="Question 5">
-            <md-content class="md-padding">
-              <h1 class="md-display-1">How much wood could a wood chuck chuck if a wood could chuck wood?</h1>
-              <md-radio-group flex>
-                  <md-radio-button value='1'>about all of it</md-radio-button>
-                  <md-radio-button value='2'>What's a woodchuck? A beaver? </md-radio-button>
-                  <md-radio-button value='3'>depends on the time of the year</md-radio-button>
-                  <md-radio-button value='4'>14.5</md-radio-button>
-              </md-radio-group>
-              <md-button class="md-raised md-primary btn-block">Check Answer</md-button>
-            </md-content>
-          </md-tab>
+          <div ng-controller="validate-answer">
+            
+            <md-tab ng-repeat="input in data" label="Question @{{$index+1}}">
+              <md-content class="md-padding">
+                <h1 class="md-display-1">How much wood could a wood chuck chuck if a wood could chuck wood?</h1>
+                <md-radio-group ng-disabled="isValid==true" flex ng-model="input.}">
+                    <md-radio-button ng-class="{'bg-success':answer.answer1}" name="answer1" value="1">about all of it</md-radio-button>
+                    <md-radio-button ng-class="{'bg-danger':answer.answer2}" name="answer2"  value="2">What's a woodchuck? A beaver? </md-radio-button>
+                    <md-radio-button ng-class="{'bg-danger':answer.answer3}" name="answer3"  value="3">depends on the time of the year</md-radio-button>
+                    <md-radio-button ng-class="{'bg-danger':answer.answer4}" name="answer4"  value="4">14.5</md-radio-button>
+                    <!--<span ng-bind="answers"></span>-->
+                </md-radio-group>
+                <md-button ng-click="validation(answers@{{$index}})" class="md-raised md-primary btn-block">Check Answer</md-button>
+              </md-content>
+            </md-tab> 
+            
+          </div>  <!-- end validate-answer controller -->
         </md-tabs>
       </md-content>
     </div>
