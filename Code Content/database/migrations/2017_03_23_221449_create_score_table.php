4421f93bac4dac3d4273a3b5827a2b02fba6460a
SiamakSamie <siamak.samie@gmail.com>
2017-03-24 00:20:45 -0400

    Bug Fix for Travis
    Name was incorrect for #96

--- Code Content/database/migrations/2017_03_23_221449_create_score_table.php
+++ Code Content/database/migrations/2017_03_23_221449_create_score_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateQuestionsTable extends Migration
+class CreateScoreTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,6 +29,6 @@ class CreateQuestionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('score');
     }
 }
