4648dff94b024183d693009e6d648ea32f34a892
SunXP <SunXP@users.noreply.github.com>
2017-04-01 15:58:23 -0400

    Improvement for edit quiz
    Front-end for adding and removing questions while editing. #10

--- Code Content/resources/views/editing_quiz.blade.php
+++ Code Content/resources/views/editing_quiz.blade.php
@@ -19,17 +19,11 @@
                     <input type="hidden" name="_method" value="PATCH">
                     <input type="hidden" name="_token" value="{{ Session::token() }}">
                     <input type="hidden" name="quiz_id" value="{{ $my_quiz->id }}">
-                                                                                     
-                                                    <!-- this is used as a default value for uni name, before suggesting others -->
-                    <div class="form-group" ng-controller="searchForm-controller" ng-init="selected_uni= '{{$my_quiz->university}}'">
-                        <label name="university">University name:</label>
-                        <input list="uni_names" id="university" name="university" rows="10" class="form-control" ng-model="selected_uni"
-                        ng-change="autocomplete_unis(selected_uni)" required>
-                        
-                        <datalist id="uni_names">
-                            <option ng-repeat="match in matching" value="@{{match}}"></option>
-                        </datalist>
                     
+                    <div class="form-group">
+                        <label name="university">University name:</label>
+                        <input id="university" name="university" rows="10" class="form-control"
+                        value="{{$my_quiz->university}}" required>
                     </div>
                     
                     <div class="form-group">
@@ -46,12 +40,12 @@
                     
                     <div class="form-group">
                         <label name="quizdescription">Quiz description:</label>
-                        <textarea name="quizdescription" rows="3" class="form-control" required>{!!$my_quiz->quizdescription!!}</textarea>
+                        <textarea name="quizdescription" rows="3" class="form-control" required>{{$my_quiz->quizdescription}}</textarea>
                     </div> 
                     
                     <div class="form-group">
                         <label name="quizresources">Resources:</label>
-                        <textarea name="quizresources" rows="3" class="form-control">{{$my_quiz->resources}}</textarea>
+                        <textarea name="quizresources" rows="3" class="form-control" required>{{$my_quiz->resources}}</textarea>
                     </div> 
                     
                     
@@ -91,6 +85,48 @@
                     
                     @endforeach
                    
+                   
+                   <div ng-controller = "add-forms" ng-init="initQuizId({{$question->quiz_id}})">
+                    
+                    <fieldset data-ng-repeat="question in editedQuestions">
+                        
+                        <button class="btn btn-info btn-sm" onclick = "return false" ng-show="$last" ng-click="removeEditedQuestion()" ><span class="glyphicon glyphicon-minus" ></span></button> 
+                        <br /><br />
+                        
+                        <div class="form-group">
+                            <label name="question">Question <span ng-bind="$index+lengthQuestions+1"></span>:</label>
+                            <input id="question" ng-model="question.question" name="question[@{{$index}}]" class="form-control">
+                        </div>
+                        
+                        <div class="form-group">
+                            <label name="answer">Correct Answer:</label>
+                            <input id="answer1" ng-model="question.answer1" name="answer1[@{{$index}}]" rows="10" class="form-control">
+                        </div>
+                        
+                        <div class="form-group">
+                            <label name="answer">Wrong Answer 1:</label>
+                            <input id="answer2" ng-model="question.answer2" name="answer2[@{{$index}}]" rows="10" class="form-control">
+                        </div>     
+                        
+                        <div class="form-group">
+                            <label name="answer">Wrong Answer 2:</label>
+                            <input id="answer3" ng-model="question.answer3" name="answer3[@{{$index}}]" rows="10" class="form-control">
+                        </div>
+                        
+                        <div class="form-group">
+                            <label name="answer">Wrong Answer 3:</label>
+                            <input id="answer4" ng-model="question.answer4" name="answer4[@{{$index}}]" rows="10" class="form-control">
+                        </div>
+                      
+                    </fieldset>
+                   
+                    <!--<button class="btn btn-info btn-sm" onclick="return false" ng-click="addEditedQuestion()" > <span class="glyphicon glyphicon-plus" > </span> </button>-->
+                    <button class="btn btn-info btn-sm" onclick="return false" ng-click="addEditedQuestion()" > <span class="glyphicon glyphicon-plus" > </span> </button>
+                    <br /><br />
+                
+                </div>
+                   
+  
                     <input type="submit" value="Update Quiz" class="btn btn-success btn-lg btn-block">
                     <a class="btn btn-default btn-block btn-lg" href="{{url(URL::previous())}}"> Cancel </a>
                 </form>
