053856621b2098d623e3f1023146941d50f5cce4
27490909 <danaalexandrallamas@gmail.com>
2017-04-03 21:57:55 -0400

    Updated unit testing spec file

--- Code Content/Unit Testing/jasmine/spec/preQuizSpec.js
+++ Code Content/Unit Testing/jasmine/spec/preQuizSpec.js
@@ -1,4 +1,6 @@
 describe('adding questions to a quiz', function() {
+
+
 	
   /*
   prequiz_module.controller('add-forms', function($scope){
@@ -23,9 +25,10 @@ describe('adding questions to a quiz', function() {
   //guide: https://docs.angularjs.org/guide/controller
   
   describe ('add-forms', function() {
+  	var prequiz_module = angular.module('preQuiz-module', ['ngMaterial', 'ngAnimate', 'luegg.directives']);
     var $scope;
     
-	beforeEach(module('prequiz_module'));
+	beforeEach(module('preQuiz-module'));
 	
 	beforeEach(inject(function($rootScope, $controller) {
 		$scope = $rootScope.new();
@@ -35,15 +38,55 @@ describe('adding questions to a quiz', function() {
 	it('should add a question when you click the +', function() {
 		$scope.questions.length = 1;
 		$scope.questions.addQuestions();
-		expect($scope.questions.length).toEqual(2);
+		expect($scope.newQuestion.length).toEqual(2);
 	});
 	
   });
   
-  
-  
+ /*
+ prequiz_module.controller('sidenav-controller', function($scope, $mdSidenav, $mdDialog) {
+      $scope.isSidenavOpen = false;
+      
+      // open the menu
+      $scope.openLeftMenu = function() {
+        $mdSidenav('left').toggle();
+      };
+      
+      // close the menu
+      $scope.toggleLeft = function() {
+        $mdSidenav('left').toggle();
+      };
+      
+      // display error message for users who aren't logged in
+      $scope.dispErrMsg = function(ev) {
+        $mdDialog.show(
+          $mdDialog.alert()
+          .clickOutsideToClose(true)
+          .title('Feature Unavailable')
+          .textContent('Please login before using this feature')
+          .ok('Got it!')
+          .targetEvent(ev)
+        );
+      };
+  });
+ */
 
-	
+  describe('sidenav-controller', function() {
+
+  	var $scope;
+
+  	beforeEach(inject(function($rootScope, $controller) {
+  		$scope = $rootScope.new();
+  		$controller('sidenav-controller', {$scope: $scope});
+  	}));
+
+  	it('should toggle the sidebar navigation', function() {
+  		$scope.isSidenavOpen = false;
+  		$scope.isSidenavOpen.openLeftMenu();
+  		expect($scope.isSidenavOpen = true);
+  	});
+
+  });
 	
 	
 });
