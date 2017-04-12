c74370883d3e577f5db13d33467b3a604ee8f8e8
Ryan Nichols <ryan3nichols@gmail.com>
2017-04-12 18:19:27 -0400

    Cleaned up/organized routes, created controller for contact us and a small edit to contact us view. fixes issue #97

--- Code Content/app/Http/Controllers/ContactUsController.php
+++ Code Content/app/Http/Controllers/ContactUsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
 use Illuminate\Http\Request;
 
 class ContactUsController extends Controller
@@ -9,5 +10,19 @@ class ContactUsController extends Controller
     public function contact_us()
     {
         return view('contact_us');
+    }
+    
+    public function sednContactUsMail() {
+       $data = request("message");
+       $name = request("name");
+       $email = request("email");
+       
+       Mail::send('email_message', array('name'=>$name, 'data'=>$data, 'email'=>$email), function($message) {        
+         $message->to("ryan3nichols@gmail.com", "contact us")->subject("Contact Us email from PreQuiz"); 
+         
+       });
+    
+       return Redirect('/contact_us')->with('status', 'We will get back at you eventually');
+
     }
 }
--- Code Content/routes/web.php
+++ Code Content/routes/web.php
@@ -12,26 +12,17 @@
 */
 
 Route::post('/getAllUnis', 'SearchController@getAllUnis');
-
 Route::post('/getAllCourses', 'SearchController@getAllCourses');
-
 Route::post('/getAllUserNames', 'SearchController@getAllUsernames');
-
-
 Route::post('/getQuestions', 'EditQuizController@getQuestions');
 
-Route::get('/', function () {
-    
-    return view('home');
-});
-
+Route::get('/', function () {return view('home');});
 Route::post('/search', 'SearchController@search');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-//Route::post('/comment_list', 'CommentController@getComments');
 Route::post('/lastComment', 'CommentController@rawLastComments');
 
 // all the routes in this section are restriced to logged in users only
@@ -61,19 +52,5 @@ Route::post('/updateProfileInfo', "EditProfileController@update");
 
 Route::patch('/updateQuizInfo', 'EditQuizController@update');
 
-
-Route::post('/sendContactUsMail', function() {
-   $data = request("message");
-   $name = request("name");
-   $email = request("email");
-   
-   Mail::send('email_message', array('name'=>$name, 'data'=>$data, 'email'=>$email), function($message) {        
-     $message->to("ryan3nichols@gmail.com", "contact us")->subject("Contact Us email from PreQuiz"); 
-     
-   });
-
-   return [$data,$name,$email];
-
- });  
-
+Route::post('/sendContactUsMail', 'ContactUsController@sednContactUsMail'); 
 Route::get('/notifications', 'NotificationsController@display');
